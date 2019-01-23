c1015a4a57cedad774d0e342c443747f012cac44
STeven <sanguine.steve@gmail.com>
2019-01-23 14:37:40 +0200

    Slide Toggler added to List group Item

--- gulpscss/source/js/main.js
+++ gulpscss/source/js/main.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
+
+
 	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
     $(this).toggleClass('open');
     $(".sidenav").toggleClass("sidenav--toggle");
   });
   
-  
+  $(".sidenav").toggleClass("sidenav--toggle");
+
+
+  jQuery(".list__group__item").click(function(){
+    console.log('123');
+    jQuery(this).find('.list__group__item--hidden').slideToggle();
+    });
 
 });
